16049c6468591b28d5a86fbb228282b3e88055eb
mark <mark@ip241-087.stu-wireless.HZeeland.nl>
2015-02-11 11:24:50 +0100

    wow

--- SOR2tmp/src/main/LolzController.java
+++ SOR2tmp/src/main/LolzController.java
@@ -3,10 +3,11 @@ package main;
 public class LolzController {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
 }
 // ayyyy lmao
 // test - Jesse
+// test - Mark   whatup boys
