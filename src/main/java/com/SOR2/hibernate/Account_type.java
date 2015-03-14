581fb01028dcae2e0e579b99a8641a1261249114
mark <mark@localhost.localdomain>
2015-03-14 12:06:42 +0100

    begonnen met referentieklassen op te zetten naar tabbellen voor hibernate

--- src/main/java/com/SOR2/hibernate/Account_type.java
+++ src/main/java/com/SOR2/hibernate/Account_type.java
@@ -1,5 +1,41 @@
 package com.SOR2.hibernate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account_type")
 public class Account_type {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String firstName;
+
+	public Account_type() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return firstName;
+	}
+
+	public void setName(String first_name) {
+		this.firstName = first_name;
+	}
 
 }
--- src/main/java/com/SOR2/hibernate/ID.java
+++ src/main/java/com/SOR2/hibernate/ID.java
@@ -1,5 +1,58 @@
 package com.SOR2.hibernate;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ID")
 public class ID {
+
+	// @Column(name = "messages")
+	private String message;
+
+	// @Column(name = "account_type")
+	private String accountType;
+
+	// @Column(name = "users")
+	private String firstName;
+
+	// @Column(name = "message_recipients")
+	private String message_recipients;
+
+	public ID() {
+
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMessage_recipients() {
+		return message_recipients;
+	}
+
+	public void setMessage_recipients(String message_recipients) {
+		this.message_recipients = message_recipients;
+	}
 
 }
