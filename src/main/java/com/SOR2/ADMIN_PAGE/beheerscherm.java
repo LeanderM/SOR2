eeb6ea746f565aa7ee74ba17b2780c5226c7e81d
JesseLabruyere <JesseLabruyere@gmail.com>
2015-03-21 14:44:16 +0100

    Commentaar toegevoegd aan admin page.

--- src/main/java/com/SOR2/ADMIN_PAGE/beheerscherm.java
+++ src/main/java/com/SOR2/ADMIN_PAGE/beheerscherm.java
@@ -35,38 +35,42 @@ public class beheerscherm extends WebPage {
 	}
 
 	// voor tables: http://wicket.apache.org/guide/guide/repeaters.html
+	// deze link beschrijft hoe je html tables kunt vullen met wicket
 	public void setInformation(List data) {
 
+		// Een nieuwe ArrayList
 		List<String[]> dataList = new ArrayList<String[]>();
-
+		// We vormen de bestaande List in een List met String arrays
+		// Deze zijn gemakkelijker aan een tabel toe te voegen
 		for (int i = 0; i < data.size(); i++) {
+			// object in een Map stoppen
 			Map row = (Map) data.get(i);
+			// Nieuwe String array maken
 			String[] textRow = new String[4];
+			// Alle data uit de map in de String array stoppen
 			textRow[0] = row.get("message_ID").toString();
 			textRow[1] = row.get("sender").toString();
 			textRow[2] = row.get("subject").toString();
 			textRow[3] = row.get("message").toString();
+			// Voeg de String array toe aan de dataList
 			dataList.add(textRow);
 		}
 
-		// System.out.println(dataList.toString());
-		// method loadCountriesFromCsv is defined elsewhere in the class.
-		// It reads countries data from a csv file and returns each row as an
-		// array of Strings.
-
+		// DatalistProvider object met de dataList
 		ListDataProvider<String[]> listDataProvider = new ListDataProvider<String[]>(
 				dataList);
 
-		// de TR van de table
+		// De table met de wicketid van de table rows
 		DataView<String[]> dataView = new DataView<String[]>("rows",
 				listDataProvider) {
 
+			// Geeft aan hoe iedere rij moet worden gepopuleerd
 			@Override
 			protected void populateItem(Item<String[]> item) {
 				String[] messagesArray = item.getModelObject();
-				// De TD's van de table
+				// Een rij
 				RepeatingView repeatingView = new RepeatingView("dataRow");
-
+				// Een loop om cellen aan de rij toe te voegen
 				for (int i = 0; i < messagesArray.length; i++) {
 					repeatingView.add(new Label(repeatingView.newChildId(),
 							messagesArray[i]));
@@ -75,14 +79,15 @@ public class beheerscherm extends WebPage {
 			}
 		};
 
+		// Geeft aan hoeveel items er per page moeten worden weergegeven
 		dataView.setItemsPerPage(15);
 
 		add(dataView);
 		// add(new PagingNavigator("pagingNavigator", dataView));
 	}
 
+	// Vraagt de gegevens uit de database of via de facade
 	public List retrieveInformation() {
-
 		String colom = "date, message_ID, sender, subject, message";
 		String table = "messages";
 		String whereClause = "1=1 ORDER BY date DESC";
