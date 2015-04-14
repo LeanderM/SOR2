125d58fe88176653b2aadeb542e5f8d2023571b2
JesseLabruyere <JesseLabruyere@gmail.com>
2015-04-14 14:11:06 +0200

    Redirect controlleerd user Role/Type

--- src/main/java/com/SOR2/ADMIN_PAGE/beheerschermRedirect.java
+++ src/main/java/com/SOR2/ADMIN_PAGE/beheerschermRedirect.java
@@ -3,7 +3,9 @@ package com.SOR2.ADMIN_PAGE;
 import org.apache.wicket.RestartResponseAtInterceptPageException;
 import org.apache.wicket.markup.html.WebPage;
 
+import com.SOR2.AccessDenied;
 import com.SOR2.SESSION.AuthenticatedWebPage;
+import com.SOR2.SESSION.BackendSession;
 
 /**
  * Check user Role en redirect naar juiste beheerpage
@@ -16,6 +18,19 @@ public class beheerschermRedirect extends WebPage implements
 		AuthenticatedWebPage {
 
 	public beheerschermRedirect() {
-		throw new RestartResponseAtInterceptPageException(beheerscherm.class);
+		BackendSession session = (BackendSession) getSession();
+		String userName = session.getUser();
+		String userRole;
+
+		// Check de role van de user via de facade
+		// userRole = HibernateMain.getRole(userName);
+
+		if (userRole.equals("ADMIN")) {
+			throw new RestartResponseAtInterceptPageException(
+					beheerscherm.class);
+		} else {
+			throw new RestartResponseAtInterceptPageException(
+					AccessDenied.class);
+		}
 	}
 }
