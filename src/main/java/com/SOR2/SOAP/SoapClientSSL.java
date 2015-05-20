5843935e9fe6a92a33ed5cbff1002c55ff1c02a8
leander <leander@localhost.localdomain>
2015-05-20 15:42:19 +0200

    testing ondersteuning toegevoegd (nog niet compleet) en wat opgeruimd

--- src/main/java/com/SOR2/SOAP/SoapClientSSL.java
+++ src/main/java/com/SOR2/SOAP/SoapClientSSL.java
@@ -2,13 +2,16 @@ package com.SOR2.SOAP;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import com.SOR2.ontvanger.*;
-import com.SOR2.SOAP.XMLObjects.*;
+
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
+import com.SOR2.SOAP.XMLObjects.DocumentInformation;
+import com.SOR2.SOAP.XMLObjects.Message;
+import com.SOR2.SOAP.XMLObjects.ResponseMessage;
+
 public class SoapClientSSL {
-	
+
 	private URL url;
 	private QName serviceQName;
 	private Service service;
@@ -16,27 +19,29 @@ public class SoapClientSSL {
 	private DocumentInformation documentInformation;
 	private Message message;
 	private boolean succesFull;
-	
+
 	static {
-	    //for localhost testing only
-		// anders werkt het verzenden met SSL selfsigned-certificaat niet lekker met de hostname localhost
-	    javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
-	    new javax.net.ssl.HostnameVerifier(){
- 
-	        public boolean verify(String hostname,
-	                javax.net.ssl.SSLSession sslSession) {
-	            if (hostname.equals("localhost")) {
-	                return true;
-	            }
-	            return false;
-	        }
-	    });
+		// for localhost testing only
+		// anders werkt het verzenden met SSL selfsigned-certificaat niet lekker
+		// met de hostname localhost
+		javax.net.ssl.HttpsURLConnection
+				.setDefaultHostnameVerifier(new javax.net.ssl.HostnameVerifier() {
+
+					public boolean verify(String hostname,
+							javax.net.ssl.SSLSession sslSession) {
+						if (hostname.equals("localhost")) {
+							return true;
+						}
+						return false;
+					}
+				});
 	}
- 
-	public SoapClientSSL(DocumentInformation documentInformation, Message message, String url, String nameSpace, String serviceName) {
-		
+
+	public SoapClientSSL(DocumentInformation documentInformation,
+			Message message, String url, String nameSpace, String serviceName) {
+
 		try {
-			
+
 			// false by default
 			succesFull = false;
 			// save to variable
@@ -45,32 +50,35 @@ public class SoapClientSSL {
 			this.message = message;
 			// url "https://localhost:8443/services/testreceiver?wsdl"
 			this.url = new URL(url);
-			// nameSpace "http://ontvanger.SOR2.com/" , serviceName 
-			serviceQName = new QName(nameSpace, "TestReceiver");
+			// nameSpace "http://ontvanger.SOR2.com/" , serviceName
+			serviceQName = new QName(nameSpace, serviceName);
 			// create the service using the QName and URL
-			service = Service.create(this.url, serviceQName);			
-			// We get the interface class so we can use it to see what methods are available 
-			receiver = service.getPort(com.SOR2.ontvanger.DocumentReceiver.class);
-		
+			service = Service.create(this.url, serviceQName);
+			// We get the interface class so we can use it to see what methods
+			// are available
+			receiver = service
+					.getPort(com.SOR2.ontvanger.DocumentReceiver.class);
+
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-	
+
 	}
-	
+
 	public void sendSoapCall() {
 		// we call the method
-		ResponseMessage response = receiver.sendDocument(message, documentInformation);
-		
+		ResponseMessage response = receiver.sendDocument(message,
+				documentInformation);
+
 		// if nothing was returned sending the message failed
-		if(response == null) {
+		if (response == null) {
 			succesFull = false;
 		}
-		
+
 		succesFull = response.getSuccess();
 	}
-	
-	public boolean successFull(){
+
+	public boolean successFull() {
 		return succesFull;
 	}
 }
